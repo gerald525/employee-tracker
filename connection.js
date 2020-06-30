f8b8d76e2f997257a0aafd2eec0f703ff65de26e
gerald525 <gerald.lloyd.us@gmail.com>
2020-06-30 14:20:48 -0700

    initial commit

--- connection.js
+++ connection.js
@@ -1,17 +1,11 @@
-// const mysql = require("mysql");
-// const { connect } = require("../../Routes/htmlRoutes");
-
-
-
-// const connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "root",
-//     database: "employees_db"
-// })
-
-// // create our connectioin to mysql
-// connection.connect();
-
-// module.exports = connection;
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "employees_db"
+})
+// create our connectioin to mysql
+connection.connect();
+module.exports = connection;
